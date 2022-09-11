b735cadc3912f43cb891b39413e06d09efac7831
stvnbash <56286042+stvnbash@users.noreply.github.com>
2022-09-10 23:20:17 -0700

    Added TeachPoint, Removed categories from Live Chat with Tech Office

--- components/AuthPrompt.js
+++ components/AuthPrompt.js
@@ -32,7 +32,7 @@ export default function AuthPrompt({ role }) {
                                     className="px-4 py-2 font-semibold text-md bg-ecrDarkBlue text-white rounded-full shadow-sm hover:shadow-lg hover:font-bold hover:underline hover:underline-offset-2 w-60"
                                     onClick={() => { signIn("azure-ad"); }
                                     }>
-                                    Sign in to myECR
+                                    Sign in to MyECR
                                 </button>
                             </div>
                         }
